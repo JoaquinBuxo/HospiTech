cd11624e2aaa812874446d9bec39251a323af964
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-01 17:19:24 +0100

    setup server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import router from './router.js';
+
+const PORT = 4000;
+
+const app = new express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
