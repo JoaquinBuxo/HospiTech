f33a25697858f8b5a4c3c3f4262e8bacf57ac0bb
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-02 13:21:46 +0100

    fix: import controller in router

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as hospitalController from './controllers/hospital.controller.js';
-import * as equipmentController from './controllers/equipment.controller';
-import * as userController from './controllers/user.controller';
+import * as equipmentController from './controllers/equipment.controller.js';
+import * as userController from './controllers/user.controller.js';
 
 const router = Router();
 
