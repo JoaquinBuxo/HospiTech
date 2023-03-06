5146b25e71d3ac2ed3790f4a4a66b8f426a3184a
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-06 20:57:37 +0100

    Style: Improve view FilterBar component

--- client/src/components/FilterBar/FilterBar.jsx
+++ client/src/components/FilterBar/FilterBar.jsx
@@ -1,8 +1,57 @@
 import React from 'react';
+
+import { FormControl, Grid, Autocomplete, FormLabel } from '@mui/joy';
+import { Search, LocalHospital, FilterList } from '@mui/icons-material';
+
 import './FilterBar.css';
 
 const FilterBar = () => {
-  return <div className='filter-bar'>filter bar</div>;
+  const hospitals = ['test'];
+  const equipments = ['test'];
+  const order = ['First Added', 'Last Added'];
+
+  return (
+    <Grid
+      container
+      direction='row'
+      justifyContent='center'
+      alignItems='center'
+      spacing={2}
+      sx={{ margin: 0 }}
+      className='filter-bar'
+    >
+      <Grid xs={3}>
+        <FormControl>
+          <FormLabel>Search</FormLabel>
+          <Autocomplete
+            startDecorator={<Search />}
+            placeholder='Search Equipment'
+            options={equipments}
+          />
+        </FormControl>
+      </Grid>
+      <Grid xs={3}>
+        <FormControl>
+          <FormLabel>Order by</FormLabel>
+          <Autocomplete
+            startDecorator={<FilterList />}
+            placeholder='Order by...'
+            options={order}
+          />
+        </FormControl>
+      </Grid>
+      <Grid xs={3}>
+        <FormControl>
+          <FormLabel>Filter Hospital</FormLabel>
+          <Autocomplete
+            startDecorator={<LocalHospital />}
+            placeholder='Hospital'
+            options={hospitals}
+          />
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default FilterBar;
