235e2d7d95d255f4f9f48f244f75bb5df5ed3a8a
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-08 11:13:08 +0100

    Styles: Authentication login

--- client/src/Auth/withAuth.jsx
+++ client/src/Auth/withAuth.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { CircularProgress } from '@mui/joy';
+import '../App.css';
 
 const withAuth = (Component) => {
   return (props) => {
     const Auth = useAuth0();
 
     if (Auth.isLoading) {
-      return <div>Loading ...</div>;
+      return (
+        <div className='login-loading'>
+          <CircularProgress />
+        </div>
+      );
     }
 
     if (!Auth.isAuthenticated) {
       return (
-        <div>
-          You are not logged!
-          <button onClick={() => Auth.loginWithRedirect()}>Log In</button>
+        <div className='logout-container'>
+          <div className='logout'>
+            You are not logged!
+            <button onClick={() => Auth.loginWithRedirect()}>Log In</button>
+          </div>
         </div>
       );
     }
