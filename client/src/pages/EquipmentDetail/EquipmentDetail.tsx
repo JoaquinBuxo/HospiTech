46d00884c82ff214e2ecd7ed66506e713762ee10
Josh McCarthy <joshcaterham@gmail.com>
2023-03-14 16:27:10 +0100

    feat: convert EquipmentDetail to typescript

--- client/src/pages/EquipmentDetail/EquipmentDetail.tsx
+++ client/src/pages/EquipmentDetail/EquipmentDetail.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
 import withAuth from '../../Auth/withAuth';
+import { AuthProp } from '../../Typescript-Interfaces/Types';
 
 import Navbar from '../../components/Navbar/Navbar';
 import EquipmentDetailInner from './EquipmentDetailInner';
 
-const EquipmentDetail = ({ Auth }) => {
+type Props = {
+  Auth: AuthProp;
+};
+
+const EquipmentDetail = ({ Auth }: Props) => {
   return (
     <>
       <Navbar Auth={Auth}></Navbar>
